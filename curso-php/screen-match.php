f1be87ee6384f91085c1d003aca233e2f8d3d114
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-05-30 21:09:25 -0300

    commit 4

--- curso-php/screen-match.php
+++ curso-php/screen-match.php
@@ -3,18 +3,57 @@
 echo "Bem-vindo(a) ao screen match!" . PHP_EOL;
 
 $nomeFilme = "Top Gun - Maverick";
+// $nomeFilme = "Thor: Ragnarok";
+// $nomeFilme = "Se beber não case";
+
 $anoLancamento = 2022;
+// $anoLancamento =  $argv[1] ?? 2024;
 // $notaFilme = 8.8;
 
-$somaDeNotas = 9;
-$somaDeNotas += 6;
-$somaDeNotas += 8;
-$somaDeNotas += 7.5;
-$somaDeNotas += 5;
-$notaFilme = $somaDeNotas / 5;
+// $somaDeNotas = 9;
+// $somaDeNotas += 6;
+// $somaDeNotas += 8;
+// $somaDeNotas += 7.5;
+// $somaDeNotas += 5;
+// $notaFilme = $somaDeNotas / 5;
+
+$quantidadeDeNotas = $argc - 1;
+$somaDeNotas = 0;
+
+for ($i=0; $i < $argc; $i++) { 
+  $somaDeNotas += (float) $argv[$i];
+}
+
+// $somaDeNotas += $argv[1];
+// $somaDeNotas += $argv[2];
+// $somaDeNotas += $argv[3];
+
+$notaFilme = $somaDeNotas / $quantidadeDeNotas;
 
 $planoPrime = true;
 $incluidoNoPlano = $planoPrime && $anoLancamento < 2020;
 
+// $nomeFilme = $argv[2];
+// $notaFilme =  $argv[3];
+
 echo $nomeFilme . PHP_EOL;
+echo $anoLancamento . PHP_EOL;
 echo $notaFilme . PHP_EOL;
+
+if ($anoLancamento > 2022) {
+  echo "Esse filme é um lançamento" . PHP_EOL;
+} elseif($anoLancamento > 2020 && $anoLancamento <= 2022) {
+  echo "Esse filme ainda é novo" . PHP_EOL;
+} else {
+  echo "Esse filme não é um lançamento" . PHP_EOL;
+}
+
+$genero = match($nomeFilme) {
+  "Top Gun - Maverick" => "ação",
+  "Se beber não case" => "comédia",
+  "Thor: Ragnarok" => "super-herói",
+  default => "gênero desconhecido",
+};
+
+echo "O gênero do filme é: $genero" .  PHP_EOL;
+echo $argc;
